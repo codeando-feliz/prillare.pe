c3c9469e3fbf12e6947a2963df7dc8c8a88f5739
piratelicorne <thecodeloops@gmail.com>
2022-03-11 22:41:44 -0500

    adding loading and message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,12 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import { listProducts } from "../actions/productAction";
-
-/*
-  TODO: 'products' should be global state (Redux)
-  useEffect to get from the backend
-*/
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -22,9 +19,9 @@ const HomeScreen = () => {
     <>
       <h1>Productos Recientes</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader/>
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
